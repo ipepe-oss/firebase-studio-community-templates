ef4a3f85ad45469503d87c66c632cd9462ca4da5
ipepe.pl <ipepe.pl@gmail.com>
2025-04-14 23:57:05 +0000

    Added ruby o rails template

---
+++ ruby-on-rails/README.md
@@ -0,0 +1,32 @@
+# Ruby on Rails
+
+[![Open in IDX](https://cdn.dev/button)](https://github.com/ipepe-oss/firebase-studio-community-templates.git/tree/main/ruby-on-rails)
+
+A Ruby on Rails template with Ruby 3.1.4, PostgreSQL, and Redis.
+
+## Get Started
+
+To get started with this template, follow these steps:
+
+1.  Clone the repository.
+2.  Navigate to the project directory.
+3.  Run `bundle install` to install the required Ruby gems.
+4.  Run `yarn install` to install the required JavaScript packages.
+5.  Set up the database by running:
+```
+bash
+    rails db:create
+    rails db:migrate
+    
+```
+6.  Start the development server by running:
+```
+bash
+    ./bin/dev
+    
+```
+7.  Open your browser and navigate to `http://localhost:3000`.
+
+## Learn More
+
+-   [Ruby on Rails Documentation](https://rubyonrails.org/)
---
+++ ruby-on-rails/idx-template.nix
@@ -0,0 +1,41 @@
+nix
+{ pkgs }:
+
+let
+  rubyVersion = "3_1";
+  ruby = pkgs."ruby_${rubyVersion}";
+in {
+  name = "rails";
+  config = {
+    description = "Ruby on Rails configuration with Ruby 3.1.4, PostgreSQL, and Redis.";
+    devshell = {
+      packages = [
+        ruby
+        ruby.pkgs.bundler
+        pkgs.yarn
+        pkgs.postgresql
+        pkgs.redis
+        pkgs.git
+      ];
+
+      env = {
+        BUNDLE_IGNORE_CONFIG = "true";
+        PATH = "${pkgs.ruby.pkgs.ruby-lsp}/bin:${ruby}/bin:${pkgs.yarn}/bin:$PATH";
+        GEM_HOME = "$PROJECT_ROOT/.gem_home";
+        BUNDLE_APP_CONFIG = "$PROJECT_ROOT/.bundle_config";
+      };
+
+      init = ''
+        bundle install
+        yarn install
+        createdb || true
+        rails db:migrate
+        rails server
+      '';
+
+      scripts = {
+        server.exec = "rails server";
+      };
+    };
+  };
+}
